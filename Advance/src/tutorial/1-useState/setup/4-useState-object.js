f6dea025a1f80c6d3afa1cfb4e4a221a291f94b5
atlas25git <atlas.sarthak@gmail.com>
2021-06-07 04:22:24 +0530

    5.32

--- Advance/src/tutorial/1-useState/setup/4-useState-object.js
+++ Advance/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person,setPerson] = useState({
+    name:"atlas",
+    age: 12,
+    message: 'random'
+  });
+
+  const changeMessage = () => {
+    setPerson({...person, message:'Spread Operator makes this possible'});
+  }
+
+  return (
+    <React.Fragment>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        Change
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateObject;
--- Advance/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ Advance/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,27 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log('useEffect');
+    window.addEventListener('resize', checkSize);
+    return () => {
+      console.log('cleanup');
+      window.removeEventListener('resize', checkSize);
+    };
+  }, []);
+  console.log('render');
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
